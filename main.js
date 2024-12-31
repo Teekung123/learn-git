1b622048f748e6d7184e9078101bb330c7b2e14b
Teekung123 <teehid099@gmail.com>
2024-12-31 14:03:39 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+document.getElementById("submit").addEventListener("click",function() {
+    let name = document.getElementById("name").value;
+
+    alert(`สวัสดี, ${name}! Happy New Year 2025!`);
+});
