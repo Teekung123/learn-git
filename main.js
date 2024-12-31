ca2dc093e30c2c9acef45fed7935e11eba29d687
Teekung123 <teehid099@gmail.com>
2024-12-31 19:47:47 +0700

    first commit

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 document.getElementById("submit").addEventListener("click",function() {
     let name = document.getElementById("name").value;
-
-    alert(`สวัสดี, ${name}! Happy New Year 2025!`);
+    console.log(name);
+    localStorage.setItem("Username",name);
+    event.preventDefault(); // ป้องกันการโหลดหน้าซ้ำ
+    window.location.href = "index2.html"; // เปลี่ยนหน้าไปที่หน้าใหม่
 });
+
